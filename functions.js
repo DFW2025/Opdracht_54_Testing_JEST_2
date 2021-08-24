38dae459089d4fa48561782dd9c55bced7882508
DFW2025 <81567386+DFW2025@users.noreply.github.com>
2021-08-24 12:41:39 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,33 @@
+// JAVASCRIPT
+
+// Add 1
+
+// const addOne = function (numbers) {
+//     return numbers.map(number => number + 1);
+// };
+
+// module.exports = addOne;
+
+
+
+//----------------------------------------------------------------
+
+// // Words Length
+
+// const getWordLengths = function (someWords) {
+//     //Schrijf je functie...
+//     return someWords.map(word => word.length);
+// };
+
+// module.exports = getWordLengths;
+
+//----------------------------------------------------------------
+
+// Find needle
+
+const findNeedle = function (words, wordToBeFound) {
+    //Schrijf je functie...
+    return words.indexOf(wordToBeFound);
+};
+
+module.exports = findNeedle;
